595cf77e1209e47cefe0b9dbb1485ab965d57c41
Omar Atri <atriomar00@gmail.com>
2025-11-03 04:53:26 +0100

    Translate the integrity page

--- src/components/policies/integrity/IntegrityPolicy.js
+++ src/components/policies/integrity/IntegrityPolicy.js
@@ -7,62 +7,64 @@ function IntegrityPolicy() {
       <div className="w-full px-8 py-10 max-w-7xl mx-auto">
         <FadeUpOnScroll>
           <h1 className="font-poppins font-bold text-3xl md:text-4xl text-center md:text-left text-[#9dbc98] capitalize transition-all duration-1000 ">
-            Integrity policy:
+            Política de integridad:
           </h1>
         </FadeUpOnScroll>
         <FadeUpOnScroll>
           <p className="pt-6 font-open font-normal text-base text-center md:text-left text-slate-900">
-            We do it all for the halal consumer, according to Halal Correct Certification-Group's
-            motto. Employees at Halal correct Germany contribute to this philosophy,
-            which is something to be proud of. The four additional work concepts
-            that Halal correct Germany has particularly named as being the cornerstone
-            of Halal correct Germany are "Confidence in workmanship," "Consciously
-            ethical," "Improving and learning continuously," and "Objective
-            approach." Halal correct Germany must be trustworthy to consumers. In many
-            ways, Halal consumers depend on the Halal review. The Halal correct Germany
-            organization must be moral as a result. This calls for Halal correct Germany
-            to conduct its business with integrity and dependability and to
-            treat its customers fairly and respectfully.
+            Lo hacemos todo por el consumidor halal, según el lema de Halal
+            Correct Certification-Group. Los empleados de Halal correct Spain
+            contribuyen a esta filosofía, algo de lo que enorgullecerse. Los
+            cuatro conceptos de trabajo adicionales que Halal correct Spain ha
+            definido como pilares de la empresa son: &quot;Confianza en el
+            trabajo&quot;, &quot;Ética consciente&quot;, &quot;Mejora y
+            aprendizaje continuos&quot; y &quot;Enfoque objetivo&quot;. Halal
+            correct Spain debe ser confiable para los consumidores. En muchos
+            sentidos, los consumidores halal dependen de la revisión halal. Por
+            lo tanto, la organización de Halal correct Spain debe ser ética.
+            Esto exige que Halal correct Spain gestione sus negocios con
+            integridad y fiabilidad, y que trate a sus clientes de forma justa y
+            respetuosa.
           </p>
           <ul className="px-8 pt-6 font-open font-normal text-base list-disc">
             <li>
-              Integrity and impartiality:
+              Integridad e imparcialidad:
               <br />
               <br />
-              Halal correct Germany Certification (Halal correct Germany Certification)
-              guarantees its impartiality policy and confidentiality with
-              respect to the delivery of all our services. The Halal correct Germany
-              Certification managers and auditors are thoroughly screened and
-              trained to ensure their integrity, impartiality, and
-              confidentiality. The Executive Board and employees of Halal
-              Correct Certification have no personal connections with the
-              companies we certify, nor do they have any commercial or financial
-              interest in our certified companies.
+              La Certificación Halal Correct Spain (Halal Correct Spain
+              Certification) garantiza su política de imparcialidad y
+              confidencialidad en la prestación de todos nuestros servicios. Los
+              gerentes y auditores de La Certificación Halal Correct Spain son
+              evaluados y capacitados exhaustivamente para garantizar su
+              integridad, imparcialidad y confidencialidad. El Comité Ejecutivo
+              y los empleados de la Certificación Halal Correct no tienen
+              vínculos personales con las empresas que certificamos, ni ningún
+              interés comercial o financiero en ellas.
               <br />
               <br />
-              All information about our financial and operational transactions
-              with our clients are recorded, and such records are also available
-              for inspection by our independent impartiality and confidentiality
-              committee.
+              Toda la información sobre nuestras transacciones financieras y
+              operativas con nuestros clientes se registra, y dichos registros
+              también están disponibles para su inspección por parte de nuestro
+              comité independiente de imparcialidad y confidencialidad.
             </li>
             <li className="pt-3">
-              Commission to guarantee Impartiality and Confidentiality:
+              Comisión para garantizar la imparcialidad y la confidencialidad:
               <br />
               <br />
-              Halal Correct Germany is supervised by an
-              independent committee of balanced representation of interested
-              stakeholders in the sector who test and guarantee the impartiality
-              and confidentiality of the organization.
+              Halal Correct Spain está supervisada por un comité independiente
+              con una representación equilibrada de las partes interesadas del
+              sector, que evalúa y garantiza la imparcialidad así como la
+              confidencialidad de la organización.
               <br />
               <br />
-              To read more kindly{" "}
+              Para obtener más información, haz clic aquí.{" "}
               <a
                 href={`${process.env.GATSBY_APP_URL}ES-003 Appeals and complaints handling procedure.pdf`}
                 target="_blank"
                 rel="noreferrer"
                 className="text-blue-600 hover:text-blue-800 transition duration-300"
               >
-                click here
+                haz clic aquí
               </a>
               .
             </li>
