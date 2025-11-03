1cea0b19da61789c794280edb89ba0c75a8048cc
Omar Atri <atriomar00@gmail.com>
2025-11-03 04:31:20 +0100

    Translate the certification categories page

--- src/components/products-categories/ProductCategDesc.js
+++ src/components/products-categories/ProductCategDesc.js
@@ -7,16 +7,19 @@ function ProductCategDesc() {
       <div className="w-full px-8 py-10 max-w-w1300 mx-auto">
         <FadeUpOnScroll>
           <h3 className="font-poppins font-bold text-3xl md:text-4xl text-center md:text-left text-primary capitalize transition-all duration-1000 ">
-            Product categories:
+            Categorías de productos:
           </h3>
         </FadeUpOnScroll>
         <FadeUpOnScroll>
           <div className="pt-8 flex flex-col items-center md:flex-row gap-8">
             <p className="font-open text-base text-center md:text-left text-slate-900 font-semibold">
-              Halal Correct Germany is the partner with a strong global network that has more than 20 years of experience in serving producers and suppliers in the Halal market.
+              Halal Correct Spain es un socio con una sólida red global y más de
+              20 años de experiencia atendiendo a productores y proveedores del
+              mercado Halal.
               <br />
               <br />
-              Halal Correct Germany currently certifies the following products:
+              Halal Correct Spain certifica actualmente los siguientes
+              productos:
             </p>
           </div>
         </FadeUpOnScroll>
