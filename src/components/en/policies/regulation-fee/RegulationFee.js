d1e10605537ba1779f6e7904800fff5a66775205
Omar Atri <atriomar00@gmail.com>
2025-12-08 06:30:49 +0100

    Add policies fee regulation translation

---
+++ src/components/en/policies/regulation-fee/RegulationFee.js
@@ -0,0 +1,233 @@
+import React from "react"
+import ScrollAnimation from "react-animate-on-scroll"
+
+export default function RegulationFee() {
+  return (
+    <div className="w-full">
+      <div className="w-full px-8 py-10 max-w-7xl mx-auto">
+        <ScrollAnimation animateIn="animate__fadeInUp">
+          <h1 className="font-poppins font-bold text-3xl md:text-4xl text-center md:text-left text-[#9dbc98] capitalize transition-all duration-1000 ">
+            Fee Regulation:
+          </h1>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="animate__fadeInUp" delay={1} duration={2}>
+          <p className="pt-6 font-open font-normal text-base text-center md:text-left text-slate-900">
+            <span className="font-bold">Introduction:</span> Halal Correct Spain
+            is a highly recognized certification body and is already accredited
+            by several accreditation/approval bodies, such as the Emirates
+            International Accreditation Centre (EIAC), the Saudi Food and Drug
+            Authority (SFDA) and the Ministry of Industry and Advanced
+            Technology (MOIAT). This recognition allows you to expand your
+            certification services through numerous companies in Germany and
+            Europe. To briefly explain how Halal Correct Spain clients will
+            manage certification costs, Halal Correct Spain has created this fee
+            regulation system. This description seeks to maximize transparency
+            in certification processes and procedures. Therefore, to initiate
+            the certification process, it is important to consider the
+            components that Halal Correct Spain mentions below to determine an
+            estimated price.
+          </p>
+          <ul className="px-8 pt-6 font-open font-normal text-base list-disc">
+            <li>
+              Halal Correct Spain price components:
+              <br />
+              <br />
+              <div className="grid grid-cols-12 divide-y">
+                <div className="col-span-6 font-bold p-2">Components</div>
+                <div className="col-span-6 font-bold p-2 !border-t-0">
+                  Price
+                </div>
+                <div className="col-span-6 p-2">
+                  <div>- Administrative expenses</div>
+                  <div>- Local/international certification process</div>
+                </div>
+                <div className="col-span-6 p-2">250-500 €</div>
+                <div className="col-span-6 p-2">
+                  <div>- Company location and size</div>
+                  <div>- Scope category</div>
+                  <div>- Number of certified products</div>
+                  <div>- Company risk</div>
+                </div>
+                <div className="col-span-6 p-2">1450-4500 €</div>
+                <div className="col-span-6 p-2">
+                  - Location (Travel cost/time) (point 3)
+                </div>
+                <div className="col-span-6 p-2">
+                  Depending on travel distance
+                </div>
+                <div className="col-span-6 p-2">
+                  <div>
+                    - Extension of raw materials/certified product screening
+                    products
+                  </div>
+                  <div>- Annex separation</div>
+                </div>
+                <div className="col-span-6 p-2">50-150 €</div>
+                <div className="col-span-6 p-2">- Inspection expenses</div>
+                <div className="col-span-6 p-2">60 € per hour</div>
+              </div>
+              <br />
+              <br />
+            </li>
+            <div className="pt-3">
+              <span className="font-bold">1. Location (Travel cost)</span>
+              <br />
+              <div>
+                Each client has a different manufacturing location. Distance is
+                calculated according to the location of Halal Correct Spain's
+                headquarters.
+              </div>
+              <div>
+                There are also clients located outside Germany; however, this
+                does not imply that the price is much higher. Other components
+                influence the fee that is charged. Therefore, location is
+                considered one of the factors that affects the fee due to Halal
+                Correct Spain's expenses in transportation to reach the company,
+                accommodation, audit days and auditor fees.
+              </div>
+              <div>
+                For example, a client located in Germany has much easier access
+                than one located in Norway. This will increase travel costs and
+                other components mentioned above.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">2. Company location and size</span>
+              <br />
+              <div>
+                Some companies have more than one manufacturer or production
+                plant. In this case, more time is required to perform the audit.
+                This also applies to larger companies, generally
+                multinational/international ones.
+              </div>
+
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">3. Scope category</span>
+
+              <br />
+              <div>
+                There are 5 categories of products certified by Halal Correct
+                Spain HALAL CONFIRMATION CENTER LIMITED: Category CI (Processing
+                of perishable animal products), CII (Processing of perishable
+                plant products), CIII (Processing of perishable animal and plant
+                products (mixed products), CIV (Processing of shelf-stable
+                products), CV (Animal slaughter), K (Production of
+                (bio)chemicals). Categories CV and CI may have a much higher fee
+                because post-audit activities could exist. Processed meat
+                products and animal slaughter could require additional
+                inspection, meaning it would not only take a common one or two
+                day audit. Animal slaughter also requires a slaughterer and
+                supervisor during activities. The report must be completed and
+                reviewed to ensure all halal requirements are met. Additionally,
+                various companies intend to certify batch production; this type
+                of certification has a different cost (for each batch).
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">4. Inspection fee</span>
+
+              <br />
+              <div>
+                Halal correct Spain may have high-risk companies that generally
+                require additional inspection. Inspection is necessary to
+                supervise the company and ensure it still applies halal
+                procedure after the certification process.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">
+                5. Number of certified products/raw materials/screening products
+              </span>
+
+              <br />
+              <div>
+                After submitting the application form and origin verification,
+                Halal correct Spain will review and verify products and raw
+                materials. Generally, the applicant is asked to send raw
+                material specifications, flow diagram (if necessary) and halal
+                certificate of raw materials.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">6. Company risk</span>
+              <br />
+              <div>
+                There are low, medium and high risk companies. High-risk
+                companies usually require much more effort, in addition to more
+                time for review and audit. They use animal/pork raw materials or
+                alcoholic products. Company complexity could affect the
+                certification process.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">
+                7. Local/international certification process
+              </span>
+              <br />
+              <div>
+                For products certified within the country, complexity is lower
+                than for export products, especially for exporting the product
+                to Gulf countries. The company generally needs a special
+                endorsement letter/document for the specific country to which it
+                wants to send its products. This endorsement letter will be
+                provided by Halal correct Spain.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">8. Administrative costs</span>
+              <br />
+              <div>
+                The administrative assistant must perform other administrative
+                procedures. Therefore, a specific fee will be applied for this
+                activity. Administrative work is part of the internal process. A
+                specific fee will be applied to cover costs from initiation to
+                certificate issuance.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">
+                9. Extension of certified products
+              </span>
+              <br />
+
+              <div>
+                After the audit, the company usually requests changes to the
+                issued certificate. A common request is to add a new raw
+                material or product to their halal certificate. Other requests
+                may be approval to use new raw materials in halal certified
+                products. When the client requests to add a new raw material or
+                product to their halal certificate, a new certificate is
+                printed. This activity also has a different cost.
+              </div>
+              <br />
+            </div>
+            <div className="pt-3">
+              <span className="font-bold">10. Annex separation</span>
+              <br />
+
+              <div>
+                Generally, a client has one certificate and all halal certified
+                products are mentioned on the annex page; however, other
+                companies may do it differently. The client's request requires
+                mentioning only one product in one annex. This means that if
+                Halal correct Spain clients own around 10 halal certified
+                products, approximately 10 annex pages must be created. The
+                certification price is affected by this request.
+              </div>
+              <br />
+            </div>
+          </ul>
+        </ScrollAnimation>
+      </div>
+    </div>
+  )
+}
